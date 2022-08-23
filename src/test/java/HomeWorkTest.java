e33467cb0573aea72be88f4ad430e589fc9073d3
tatiparty <t.nemikyna@gmail.com>
2022-08-23 21:14:45 +0300

    moving all to pages

--- src/test/java/HomeWorkTest.java
+++ src/test/java/HomeWorkTest.java
@@ -6,13 +6,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.MainPage;
+import pages.ProfilePage;
 import webDriverFactory.WebDriverFactory;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Properties;
 
 public class HomeWorkTest {
@@ -26,10 +24,6 @@ public class HomeWorkTest {
     private org.apache.logging.log4j.Logger logger = LogManager.getLogger(Logger.class);
 
     private WebDriver driver;
-
-    private By firstName = By.id("id_fname");
-    private By secondName = By.id("id_lname");
-    private By blogName = By.id("id_blog_name");
 
     @Before
     public void setUp(){
@@ -54,66 +48,30 @@ public class HomeWorkTest {
 
     @Test
     public void testDataSaving(){
-        //Войти в Личный кабинет
+        //Войти в Личный кабинет, заполнить и сохранить данные
         String firstTitle = new MainPage(driver).
                 openUrl(url).
                 goToAuth().
                 auth(login, password).
                 enterToProfile().
-                getStatus();
+                fillAndSaveData();
 
-        Assert.assertEquals(firstTitle, "Личный кабинет");
-
-        //Заполнить и сохранить данные
-        fillAndSaveData();
+        Assert.assertEquals(firstTitle, "Данные успешно сохранены");
 
         //Открыть https://otus.ru в "чистом браузере"
         driver.quit();
         driver = new ChromeDriver();
         driver.get(url);
 
-        //Повторно войти в Личный кабинет
-        String secondTitle = new MainPage(driver).
+        //Повторно войти в Личный кабинет и проверить данные
+        ProfilePage secondTitle = new MainPage(driver).
                 openUrl(url).
                 goToAuth().
                 auth(login, password).
                 enterToProfile().
-                getStatus();
-
-        Assert.assertEquals(secondTitle, "Личный кабинет");
-
-        //Проверить данные
-        checkData();
+                checkData();
 
         logger.info(driver.manage().getCookies());
     }
 
-    public void fillAndSaveData(){
-        enterToTextArea(getElement(firstName), "Тест");
-        enterToTextArea(getElement(secondName), "Тест");
-        enterToTextArea(getElement(blogName), "Тест");
-
-        driver.findElement(By.xpath("//button[@name = 'continue']")).click();
-
-    }
-
-    public void checkData(){
-        checkTextInTextArea(getElement(firstName), "Тест");
-        checkTextInTextArea(getElement(secondName), "Тест");
-        checkTextInTextArea(getElement(blogName), "Тест");
-    }
-
-    private void enterToTextArea(WebElement element, String text){
-        element.clear();
-        element.sendKeys(text);
-    }
-
-    private void checkTextInTextArea(WebElement element, String expectedText){
-        Assert.assertEquals(expectedText, element.getAttribute("value"));
-    }
-
-    private WebElement getElement(By locator){
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-    }
 }
--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -1,7 +1,13 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ProfilePage {
     public ProfilePage(WebDriver driver) {
@@ -10,9 +16,37 @@ public class ProfilePage {
 
     private WebDriver driver;
 
-    private By title = By.xpath("//h1[@class = 'title__text']");
+    private By title = By.xpath("//div[contains(@class, 'container-padding-top-half')]/span/span");//переписать на "Данные успешно сохранены"
+    private By firstName = By.id("id_fname");
+    private By secondName = By.id("id_lname");
+    private By blogName = By.id("id_blog_name");
 
-    public String getStatus(){
+
+    public String fillAndSaveData(){
+        enterToTextArea(getElement(firstName), "Тест");
+        enterToTextArea(getElement(secondName), "Тест");
+        enterToTextArea(getElement(blogName), "Тест");
+
+        driver.findElement(By.xpath("//button[@name = 'continue']")).click();
         return driver.findElement(title).getText();
+    }
+    public ProfilePage checkData(){
+        checkTextInTextArea(getElement(firstName), "Тест");
+        checkTextInTextArea(getElement(secondName), "Тест");
+        checkTextInTextArea(getElement(blogName), "Тест");
+        return new ProfilePage(driver);
+    }
+
+    private void enterToTextArea(WebElement element, String text){
+        element.clear();
+        element.sendKeys(text);
+    }
+    private void checkTextInTextArea(WebElement element, String expectedText){
+        Assert.assertEquals(expectedText, element.getAttribute("value"));
+    }
+
+    private WebElement getElement(By locator){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
